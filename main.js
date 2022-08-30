b544daf2345f96f621e01818c18222154eab11dc
trollar <70556971+trollar@users.noreply.github.com>
2022-08-30 22:29:04 +0100

    Handle themes

--- main.js
+++ main.js
@@ -14,6 +14,20 @@ const createWindow = () => {
     )
 
     window.loadFile('index.html')
+
+
+  ipcMain.handle('dark-mode:toggle', () => {
+    if (nativeTheme.shouldUseDarkColors) {
+      nativeTheme.themeSource = 'light'
+    } else {
+      nativeTheme.themeSource = 'dark'
+    }
+    return nativeTheme.shouldUseDarkColors
+  })
+
+  ipcMain.handle('dark-mode:system', () => {
+    nativeTheme.themeSource = 'system'
+  })
 }
 
 app.whenReady().then(() => {
