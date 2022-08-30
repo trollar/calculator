a161c32d9cea041cd226d98aeb78629af27af1bb
trollar <70556971+trollar@users.noreply.github.com>
2022-08-30 22:04:29 +0100

    createWindow function

--- main.js
+++ main.js
@@ -3,3 +3,15 @@
 */
 
 const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+    const window = new BrowserWindow(
+        {
+            width: 800,
+            height: 600,
+            autoHideMenuBar: true,
+        }
+    )
+
+    window.loadFile('index.html')
+}
